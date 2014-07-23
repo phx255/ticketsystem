cd861a7ec88d2f451d7544efa7e2bb185859edd2
phx255 <phx255@gmail.com>
2014-07-23 15:03:07 +0200

    New Version

---
+++ html/main.js
@@ -0,0 +1,118 @@
+﻿//modulo AngularJS
+var TicketsManagement = angular.module('TicketsManagement', []).config(function ($locationProvider) {   $locationProvider.html5Mode(true).hashPrefix('!') });
+var id;
+
+
+//Controller principal de todas las páginas
+function mainController($scope, $http) {
+    
+
+   
+    $scope.formData = {};
+    
+    // Cuando se cargue la página, pide del API de los tickets
+    
+    
+    //Busca tickets (Todos)
+    $scope.refreshTicket = function () {
+        $http.get('/api/tickets')
+            .success(function (data) {
+            $scope.tickets = data;
+            console.log(data);
+        })
+            .error(function (data) {
+            console.log('Error: ' + data);
+        });
+    };
+    
+    
+    
+    
+    //devuelve un ticket
+    $scope.GetTicket = function (id) {
+        $http.get('/api/ticket/' + id)
+    .success(function (data) {
+            
+            //ponemos valores del select2
+            $("#levelpicker").val(data.level);
+            $('.selectpicker').selectpicker('refresh');
+            
+            $scope.formData = data;
+            
+            //$('select[name=levelpicker]').val(data.level);
+            
+            
+            console.log(data.level);
+        })
+    .error(function (data) {
+            console.log('Error: ' + data);
+        });
+    };
+    
+    
+    // Añade un ticket
+    $scope.AddTicket = function () {
+        $http.post('/api/tickets', $scope.formData)
+                    .success(function (data) {
+            $scope.formData = {};
+            $scope.tickets = data;
+            $scope.refreshTicket();
+            console.log(data);
+
+        })
+
+                    .error(function (data) {
+            console.log('Error:' + data);
+        });
+
+
+    };
+    
+    
+    // Actualiza un ticket
+    $scope.UpdateTicket = function (id) {
+        
+        $http.post('/api/tickets/' + $scope.formData._id)
+                    .success(function (data) {
+            $scope.tickets = data;
+            $scope.refreshTicket();
+                      
+        })
+                    .error(function (data) {
+            console.log('Error:' + data);
+        });
+    };
+
+    
+    
+    //recoge el ticket de querystring
+    $scope.ticketId = function () {
+        
+        //parámetros querystring.
+        var id = window.location.pathname.replace('/UpdateTicket/', '');
+        if (id != null) {
+            $scope.GetTicket(id);
+        }
+        
+
+    }
+
+    
+    // Borra un ticket
+    $scope.RemoveTicket = function (id) {
+        $http.delete('/api/tickets/' + id)
+                .success(function (data) {
+            $scope.tickets = data;
+            $scope.refreshTicket();
+            console.log(data);
+        })
+                .error(function (data) {
+            console.log('Error:' + data);
+        });
+    };
+
+
+     $scope.refreshTicket();
+  
+}
+
---
+++ server.js
@@ -0,0 +1,166 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.use(express.static(__dirname + '/html'));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+
+
+var mongoose = require('mongoose');
+
+// Conexión con la base de datos
+mongoose.connect('mongodb://cycleit:cycleit@ds055709.mongolab.com:55709/cycleitapp');
+
+
+// Definición de Schema/modelos
+
+var ticketsSchema = new mongoose.Schema({
+    subject: String ,
+    datetime: Date ,
+    description:  String ,
+    username: String ,
+    level: String,
+    summary:  String
+});
+
+
+
+var ticket = mongoose.model('ticket', ticketsSchema);
+
+app.listen(3030, function () {
+    console.log("Node server running on http://localhost:3030");
+});
+
+
+
+
+
+
+
+
+
+
+// Rutas de nuestro API
+// GET de todos los Tickets
+app.get('/api/tickets', function (req, res) {
+    
+
+
+    ticket.find(function (err, tickets) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(tickets);
+    });
+});
+
+
+// Rutas de nuestro API
+// GET de todos los Tickets
+app.get('/api/ticket/:ticket', function (req, res) {
+    
+    
+    ticket.findById(req.params.ticket, function (err, ticket) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(ticket);
+    });
+    
+
+});
+
+
+
+
+// POST que crea un TODO y devuelve todos tras la creación
+app.post('/api/tickets', function (req, res) {
+    console.log(req.body);
+    ticket.create({
+        username: req.body.username,
+        datetime: req.body.datetime,
+        description: req.body.description,
+        level: req.body.level,
+        done: false
+    }, function (err, ticket) {
+        if (err) {
+            res.send(err);
+        }
+        
+        
+        res.json(ticket);
+       
+    });
+});
+
+
+app.post('/api/tickets/:id', function (req, res) {
+    
+    ticket.update({
+        
+        username: req.body.username,
+        datetime: req.body.datetime,
+        description: req.body.description,
+        level: req.body.level,
+        done: false
+    }, function (err, ticket) {
+        if (err) {
+            res.send(err);
+        }
+        
+        
+        res.json(ticket);
+       
+    });
+});
+
+
+
+// DELETE un TODO específico y devuelve todos tras borrarlo.
+app.delete('/api/tickets/:ticket', function(req, res) {
+    ticket.remove({
+        _id: req.params.ticket
+    }, function (err, ticket) {
+        if (err) {
+            res.send(err);
+        }
+        
+
+        res.json(ticket);
+    })
+});
+
+
+//rutas
+
+
+
+app.get('/', function(req, res) {
+    res.sendfile('./html/index.html');
+});
+
+app.get('/NewTicket', function (req, res) {
+    res.sendfile('./html/NewTicket.html');
+});
+
+app.get('/UpdateTicket', function (req, res) {
+    res.sendfile('./html/UpdateTicket.html');
+});
+
+
+
+app.get('/UpdateTicket/:id', function (req, res) {
+    res.sendfile('./html/UpdateTicket.html');
+});
+
+
+
